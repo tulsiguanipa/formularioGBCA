5925cc5723025ac7f5f6fda613018588378854dd
Tulsi Guanipa <tulsiguanipa@gmail.com>
2020-04-26 21:29:42 -0300

    historial

--- src/app/formulario/ingreso/ingreso.component.ts
+++ src/app/formulario/ingreso/ingreso.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
-import { Nombre, Sector } from '../../models/model.interface';
+import { Nombre, Sector, Historial } from '../../models/model.interface';
 import { VisitasService } from '../../services/visitas.service';
+import { historial } from 'src/app/constants/data';
 
 @Component({
   selector: 'app-ingreso',
@@ -13,6 +14,7 @@ export class IngresoComponent implements OnInit {
   public selectedSector: Sector = { id: 0, sector: ' '};
   public area: Sector[];
   public visito: Nombre[];
+  private historial: Historial[] = [];
 
 
   fecha = new Date();
@@ -27,6 +29,7 @@ export class IngresoComponent implements OnInit {
 
   ngOnInit(): void {
     this.area = this.visitas.getSector();
+    this.visito = this.visitas.getNombres();
   }
 
   onSelect(id: number): void {
@@ -38,6 +41,15 @@ export class IngresoComponent implements OnInit {
       return this.myForms.get('documento').invalid && this.myForms.get('documento').touched;
   }
 
+  get visitasInvalid() {
+    return this.myForms.get('visita').invalid && this.myForms.get('visita').touched;
+  }
+
+  get sectorInvalid() {
+    return this.myForms.get('sector').invalid && this.myForms.get('sector').touched;
+    
+  }
+
   get numTarjetaisInvalid() {
     return this.myForms.get('numeroTarjeta').invalid && this.myForms.get('numeroTarjeta').touched;
 }
@@ -45,7 +57,9 @@ export class IngresoComponent implements OnInit {
   formulario() {
     this.myForms = this.fb.group({
       documento    : ['', [Validators.required, Validators.minLength(7)]],
-      numeroTarjeta: ['', [Validators.required]]
+      numeroTarjeta: ['', [Validators.required]],
+      visita       : ['', [Validators.required]],
+      sector       : ['', [Validators.required]]
     });
 
   }
@@ -55,12 +69,21 @@ export class IngresoComponent implements OnInit {
       return Object.values(this.myForms.controls).forEach( control => {
         control.markAllAsTouched();
       });
-
     }
+    console.log(this.myForms); 
+    const nuevaVisita: Historial = {
+      id: Math.floor(Math.random() * 10000),
+      fecha: new Date(),
+      nombre: this.visito.filter((persona) => {
+        console.log(persona, Number(this.myForms.value.visita))
+        return persona.id === Number(this.myForms.value.visita)
+      })[0].nombre
+    }
+    this.historial.push(nuevaVisita)
+    this.myForms.reset()
   }
 
   consultar() {
       console.log(this.myForms);
-
   }
 }
--- src/app/models/model.interface.ts
+++ src/app/models/model.interface.ts
@@ -2,10 +2,23 @@ export interface Nombre {
     id: number;
     nombre: string;
     sectorId: number;
-
 };
 
 export interface Sector {
     id: number;
     sector: string;
+}
+
+export interface Persona {
+    apellido: string,
+    nombres: string,
+    fechaNacimiento: string,
+    mensaje: string
+}
+
+
+export interface Historial {
+    id: number,
+    fecha: Date,
+    nombre: string
 }
